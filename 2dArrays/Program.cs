9c2e99aec251b662145ea6a998a13cef8732eaf4
LAPTOP-URTGLTDP\alish <hois@protonmail.com>
2021-11-19 17:48:46 +0200

    Last exercise added

--- 2dArrays/Program.cs
+++ 2dArrays/Program.cs
@@ -8,10 +8,10 @@ namespace _2dArrays
         static void Main(string[] args)
         {
             int[,] array1 = new int[4, 2] { { 3, 2 }, { 3, 4 }, { 5, -2 }, { 7, 8 } };
-            int x, y;
-            (x,y) = ArraysCalculator.FindIndexOfMaxElement(array1);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
+            
+            int[,] result = ArraysCalculator.TransposeMatrix(array1);
+            
+            
 
 
         }
